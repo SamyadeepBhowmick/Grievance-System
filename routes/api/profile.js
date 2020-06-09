bc773f5f1c6d04ad29f9861032c6b685affbb663
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2020-06-09 21:49:54 +0530

    Remove user posts on account deletion done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile=require('../../models/Profile');
 const User=require('../../models/User');
+const Post=require('../../models/Post');
 
 router.get('/me',auth, async(req,res)=>{
     try{
@@ -103,6 +104,8 @@ router.get('/user/:user_id',async (req,res)=>{
 //To delete profile,user and posts
 router.delete('/',auth,async (req,res)=>{
     try {
+        //Remove user posts
+        await Post.deleteMany({user: req.user.id});
         //Remove profile
         await Profile.findOneAndRemove({user: req.user.id});
         //Remove user
