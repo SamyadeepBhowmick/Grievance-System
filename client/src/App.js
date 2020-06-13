9b6efe82e41e882aec3128045b3e5db776d5bf93
Samyadeep Bhowmick <samyadeep98b@gmail.com>
2020-06-13 13:27:40 +0530

    Commenting posts feature completed

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 import {loadUser} from './actions/auth'
@@ -48,6 +49,7 @@ const App = () =>{
             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
             <PrivateRoute exact path='/add-experience' component={AddExperience}/>
             <PrivateRoute exact path='/posts' component={Posts}/>
+            <PrivateRoute exact path='/posts/:id' component={Post}/>
           </Switch>
         </section>
       </Fragment>
